4184ee78bf91e02ac0d9da193eb98d8bf7a6000f
Izcar Muñoz Torrez <grupo-inco@Izcar.local>
2020-05-09 14:33:48 -0600

    create mptt models

--- file_manager_mptt/models.py
+++ file_manager_mptt/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from django.contrib.auth  import  get_user_model 
+from mptt.models import MPTTModel, TreeForeignKey
+from file_manager_mptt.utils.node_types import NODE_TYPE, FOLDER, FILE
+import uuid
 # Create your models here.
+
+## identifiers
+
+
+class FolderMptt(MPTTModel):
+
+    id = models.UUIDField(verbose_name="File Node ID", primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    parent = TreeForeignKey('self', null=True, related_name='children', on_delete=models.CASCADE)
+    type = models.IntegerField(choices=NODE_TYPE)
+    slug = models.SlugField(max_length=100)
+    owner = models.ForeignKey(get_user_model(), related_name='volumen', on_delete=models.PROTECT)
+    deleted = models.BooleanField(default=False)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+    def is_folder(self):
+        return self.type == FOLDER
+
+    def is_file(self):
+        return self.type == FILE
